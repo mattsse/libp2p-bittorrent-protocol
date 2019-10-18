7e282dc327cab485fde8874202e18827bb2703d8
mattsse <matthias.seitz@tum.de>
2019-10-18 13:58:05 +0200

    more basic type implementation

--- src/disk/file.rs
+++ src/disk/file.rs
@@ -1,8 +1,18 @@
 use sha1::Sha1;
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+#[derive(Clone, Eq, PartialEq, Hash)]
 pub struct FileEntry {
     pub file_hash: Sha1,
 
     pub offset: u64,
+}
+
+#[derive(Clone, Eq, PartialEq, Hash)]
+pub struct FileSlice {
+    /// index of the file in the torrentinfo
+    pub file_index: u32,
+    /// byte offset in the file where the range
+    pub offset: u32,
+    /// number of bytes this range is
+    pub window_size: u32,
 }
--- src/torrent.rs
+++ src/torrent.rs
@@ -1,11 +1,29 @@
+use crate::bitfield::BitField;
 use chrono::NaiveDate;
 use sha1::Sha1;
 
 #[derive(Clone, Eq, PartialEq)]
-pub struct TorrentInfo {
+pub struct Torrent {
+    /// urls of the trackers
+    pub trackers: Vec<String>,
+    pub comment: Option<String>,
+    pub created_by: Option<String>,
+    pub creation_date: Option<i64>,
+    pub encoding: Option<String>,
+    pub info: TorrentInfo,
+    pub nodes: Vec<(String, u32)>,
     /// the hash that identifies this torrent
     pub info_hash: Sha1,
+}
+#[derive(Clone, Eq, PartialEq)]
+pub enum TorrentInfo {
+    MerkleTree,
+    Dict(Info),
+}
 
+#[derive(Clone, Eq, PartialEq)]
+pub struct Info {
+    pub piece_hashes: Vec<Sha1>,
     pub comment: Option<String>,
     pub created_by: Option<String>,
     pub creation_date: Option<NaiveDate>,
@@ -19,3 +37,6 @@ pub enum TorrentType {
     I2P,
     SSL,
 }
+
+#[derive(Clone, Eq, PartialEq)]
+pub struct MerkleTree {}
