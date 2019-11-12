6329a612250cc0e887e1775d57551e0aa4a1f693
mattsse <matthias.seitz@tum.de>
2019-11-12 23:16:52 +0100

    Advance example

--- examples/bittorrent.rs
+++ examples/bittorrent.rs
@@ -1,16 +1,46 @@
+//! A basic example demonstrating torrenting over libp2p.
+//!
+//! In the first terminal window, run:
+//!
+//! ```sh
+//! cargo run --example bittorrent
+//! ```
+//!
+//! It will print the PeerId and the listening address, e.g. `Listening on
+//! Local peer id: PeerId("QmQodo6YFTB2CGTSgFHH1JLmFPmD4EwTgeXsqLGsnBGgYb")
+//! "/ip4/0.0.0.0/tcp/24345"`
+//!
+//! It will then generate some random file to seed and prints the path to the
+//! torrent file "/var/folders/l5/lprhf80000gn/T/.tmpQNd/rubbish.torrent"
+//!
+//! In the second terminal window, start a new instance of the example with:
+//!
+//! ```sh
+//! cargo run --example bittorrent -- /ip4/127.0.0.1/tcp/24345 "QmQodo6YFTB2CGTSgFHH1JLmFPmD4EwTgeXsqLGsnBGgYb" "/var/folders/l5/lprhf80000gn/T/.tmpQNd/rubbish.torrent"
+//! ```
+//!
+//! The two nodes establish a connection, negotiate the bittorent protocol
+//! and begin torrenting the demo file.
+
 use std::env;
 use std::fs::OpenOptions;
 use std::io::Write;
 
 use futures::prelude::*;
 use libp2p::{build_development_transport, identity, PeerId, Swarm};
+use libp2p_core::Multiaddr;
 use rand;
 use tempfile::tempdir;
 
 use libp2p_bittorrent_protocol::disk::NativeFileSystem;
 use libp2p_bittorrent_protocol::peer::TorrentState;
 use libp2p_bittorrent_protocol::{
-    Bittorrent, BittorrentConfig, BittorrentEvent, MetaInfo, TorrentBuilder, TorrentSeed,
+    Bittorrent,
+    BittorrentConfig,
+    BittorrentEvent,
+    MetaInfo,
+    TorrentBuilder,
+    TorrentSeed,
 };
 
 fn main() {
@@ -19,8 +49,7 @@ fn main() {
     // Create a random key for ourselves.
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
-
-    println!("peer id: {:?}", local_peer_id);
+    println!("Local peer id: {:?}", local_peer_id);
 
     // Set up a an encrypted DNS-enabled TCP Transport over the Mplex protocol
     let transport = build_development_transport(local_key);
@@ -34,21 +63,32 @@ fn main() {
     let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
 
     // Order Bittorrent to start torrenting from a peer.
-    if let Some(peer_id) = env::args().nth(1) {
-        let peer_id: PeerId = peer_id
-            .parse()
-            .expect("Failed to parse peer ID to torrent from");
+    if let Some(addr) = env::args().nth(1) {
+        let remote_addr = addr.clone();
 
-        let torrent = env::args().nth(2).expect("Demo torrent required.");
+        let to_dial = env::args().nth(2).expect("Demo torrent required.");
+
+        let to_dial = to_dial.parse().expect("Failed to parse peer ID to find");
+
+        let torrent = env::args().nth(3).expect("Demo torrent required.");
 
         let torrent = MetaInfo::from_torrent_file(torrent).expect("Failed to load torrent file");
 
         let info_hash = torrent.info_hash.clone();
+
         // add a new leech
         swarm.add_leech(torrent, TorrentState::Active);
 
         // start the handshake
-        swarm.handshake(info_hash, peer_id.clone());
+        swarm.insert_peer_candidate(to_dial, info_hash);
+
+        match addr.parse() {
+            Ok(remote) => match Swarm::dial_addr(&mut swarm, remote) {
+                Ok(()) => println!("Dialed {:?}", remote_addr),
+                Err(e) => println!("Dialing {:?} failed with: {:?}", remote_addr, e),
+            },
+            Err(err) => println!("Failed to parse address to dial: {:?}", err),
+        };
     } else {
         // generate some rubbish
 
@@ -83,10 +123,14 @@ fn main() {
         let seed = TorrentSeed::new(seed, meta_info);
 
         // add the seed
-        // TODO add dialing
         swarm.add_seed(seed, TorrentState::Active);
     };
 
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned port.
+    Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
+
+    // Use tokio to drive the `Swarm`.
+    let mut listening = false;
     // Start torrenting
     tokio::run(futures::future::poll_fn(move || {
         loop {
@@ -99,8 +143,32 @@ fn main() {
                         println!("Failed to add new seed: {:?}", err);
                     }
                 },
+                Async::Ready(Some(BittorrentEvent::HandshakeResult(res))) => match res {
+                    Ok(ok) => {
+                        println!("Handshake ok: {:?}", ok);
+                    }
+                    Err(err) => {
+                        println!("Failed to handshake: {:?}", err);
+                    }
+                },
+                Async::Ready(Some(BittorrentEvent::InterestResult(res))) => match res {
+                    Ok(ok) => {
+                        println!("interest ok: {:?}", ok);
+                    }
+                    Err(err) => {
+                        println!("interest error: {:?}", err);
+                    }
+                },
                 Async::Ready(Some(_)) => {}
-                Async::Ready(None) | Async::NotReady => break,
+                Async::Ready(None) | Async::NotReady => {
+                    if !listening {
+                        if let Some(a) = Swarm::listeners(&swarm).next() {
+                            println!("Listening on {:?}", a);
+                            listening = true;
+                        }
+                    }
+                    return Ok(Async::NotReady);
+                }
             }
         }
 
